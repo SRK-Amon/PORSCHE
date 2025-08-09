beb1bd96910f6b2e0ed3f892b5cf9c6618bfea3d
Mahmudjon <mahmudjonrahimjomnov007@gmail.com>
2025-08-09 17:38:55 +0500

    PORSCHE

--- src/products.ts
+++ src/products.ts
@@ -3,7 +3,7 @@ export const CarGallery = [
         id: 1,
         name: "911 Targa 4 GTS",
         price: "138,000.00",
-        img: "../src/assets/images/car1.png",
+        img: "./car1.png",
     },
     {
         id: 2,
