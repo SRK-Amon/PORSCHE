9d652dd507f060a57c5a37caeed958c7cc1649fa
Mahmudjon <mahmudjonrahimjomnov007@gmail.com>
2025-08-09 18:04:34 +0500

    PORSCHE

--- src/products.ts
+++ src/products.ts
@@ -1,74 +1,109 @@
+import car1 from "../src/assets/images/car1.png"
+import car2 from "/src/assets/images/car2.png"
+import car3 from "/src/assets/images/car3.png"
+import car4 from "/src/assets/images/car4.png"
+import car5 from "/src/assets/images/car5.png"
+
 export const CarGallery = [
     {
         id: 1,
         name: "911 Targa 4 GTS",
         price: "138,000.00",
-        img: "/src/assets/images/car1.png",
+        img: car1,
     },
-    {
+    {   
         id: 2,
         name: "911 Carrera S",
         price: "110,000.00",
-        img: "../src/assets/images/car2.png",
+        img: car2,
     },
     {
         id: 3,
         name: "911 GT3 RS",
         price: "192,600.00",
-        img: "../src/assets/images/car3.png",
+        img: car3,
     },
     {
         id: 4,
         name: "911 S/T",
         price: " 231,600.00",
-        img: "../src/assets/images/car4.png",
+        img: car4,
     },
     {
         id: 5,
         name: "911 Dakar",
         price: "173,000.00",
-        img: "../src/assets/images/car5.png",
+        img: car5,
     }
 ]
-
+import home1 from "/src/assets/images/home1.png";
+import home2 from "/src/assets/images/home2.png";
+import home3 from "/src/assets/images/home3.png";
+import home4 from "/src/assets/images/home4.png";
+import home5 from "/src/assets/images/home5.png";
+import home6 from "/src/assets/images/home6.png";
+import home7 from "/src/assets/images/home7.png";
+import home8 from "/src/assets/images/home8.png";
 export const HomeGallery = [
-    { id: 1, img: "../src/assets/images/home1.png" },
-    { id: 2, img: "../src/assets/images/home2.png" },
-    { id: 3, img: "../src/assets/images/home3.png" },
-    { id: 4, img: "../src/assets/images/home4.png" },
-    { id: 5, img: "../src/assets/images/home5.png" },
-    { id: 6, img: "../src/assets/images/home6.png" },
-    { id: 7, img: "../src/assets/images/home7.png" },
-    { id: 8, img: "../src/assets/images/home8.png" },
+    { id: 1, img: home1},
+    { id: 2, img: home2},
+    { id: 3, img: home3},
+    { id: 4, img: home4},
+    { id: 5, img: home5},
+    { id: 6, img: home6},
+    { id: 7, img: home7},
+    { id: 8, img: home8},
 ]
+import filter_colours from "/src/assets/images/filter_colours.png";
+import filter_colours_active from "/src/assets/images/filter_colours_active.png";
+import filter_wheels from "/src/assets/images/filter_wheels.png";
+import filter_wheels_active from "/src/assets/images/filter_wheels_active.png";
+import filter_materials from "/src/assets/images/filter_materials.png";
+import filter_seats from "/src/assets/images/filter_seats.png";
+import filter_options from "/src/assets/images/filter_options.png";
+import filter_technology from "/src/assets/images/filter_technology.png";
+import filter_packages from "/src/assets/images/filter_packages.png";
+import filter_accessories from "/src/assets/images/filter_accessories.png";
 
 export const filterOptions = [
-    { id: "colours", title: "Colours", img: "../src/assets/images/filter_colours.png", imgActive: "../src/assets/images/filter_colours_active.png" },
-    { id: "wheels", title: "Wheels", img: "../src/assets/images/filter_wheels.png", imgActive: "../src/assets/images/filter_wheels_active.png" },
-    { id: "materials", title: "Materials", img: "../src/assets/images/filter_materials.png" },
-    { id: "seats", title: "Seats", img: "../src/assets/images/filter_seats.png" },
-    { id: "options", title: "Options", img: "../src/assets/images/filter_options.png" },
-    { id: "technology", title: "Technology", img: "../src/assets/images/filter_technology.png" },
-    { id: "packages", title: "Packages", img: "../src/assets/images/filter_packages.png" },
-    { id: "accessories", title: "Accessories", img: "../src/assets/images/filter_accessories.png" },
+    { id: "colours", title: "Colours", img: filter_colours, imgActive: filter_colours_active },
+    { id: "wheels", title: "Wheels", img: filter_wheels, imgActive: filter_wheels_active },
+    { id: "materials", title: "Materials", img: filter_materials },
+    { id: "seats", title: "Seats", img: filter_seats},
+    { id: "options", title: "Options", img: filter_options},
+    { id: "technology", title: "Technology", img: filter_technology},
+    { id: "packages", title: "Packages", img: filter_packages},
+    { id: "accessories", title: "Accessories", img: filter_accessories},
 ];
-
+import ColoursFilter_1 from "/src/assets/images/ColoursFilter_1.png";
+import ColoursFilter_2 from "/src/assets/images/ColoursFilter_2.png";
+import ColoursFilter_3 from "/src/assets/images/ColoursFilter_3.png";
 export const ColoursFilter = [
-    { id: 1, name: "Carmine Red", img: "../src/assets/images/ColoursFilter_1.png" },
-    { id: 2, name: "Ice Grey Metallic", img: "../src/assets/images/ColoursFilter_2.png" },
-    { id: 3, name: "Arctic Grey", img: "../src/assets/images/ColoursFilter_3.png" },
+    { id: 1, name: "Carmine Red", img: ColoursFilter_1},
+    { id: 2, name: "Ice Grey Metallic", img: ColoursFilter_2 },
+    { id: 3, name: "Arctic Grey", img: ColoursFilter_3 },
 ]
+import WheelsFilter_1 from "/src/assets/images/WheelsFilter_1.png";
+import WheelsFilter_2 from "/src/assets/images/WheelsFilter_2.png";
+import WheelsFilter_3 from "/src/assets/images/WheelsFilter_3.png";
+import WheelsFilter_4 from "/src/assets/images/WheelsFilter_4.png";
+import WheelsFilter_5 from "/src/assets/images/WheelsFilter_5.png";
 export const WheelsFilter = [
-    { id: 1, name: "Carrera Swheels", img: "../src/assets/images/WheelsFilter_1.png" },
-    { id: 2, name: "Carrera Classic wheels", img: "../src/assets/images/WheelsFilter_2.png" },
-    { id: 3, name: "Carrera Exclusive Design Wheels", img: "../src/assets/images/WheelsFilter_3.png" },
-    { id: 4, name: "Spyder Design wheels", img: "../src/assets/images/WheelsFilter_4.png" },
-    { id: 5, name: "Turbo wheels", img: "../src/assets/images/WheelsFilter_5.png" }
+    { id: 1, name: "Carrera Swheels", img: WheelsFilter_1 },
+    { id: 2, name: "Carrera Classic wheels", img: WheelsFilter_2 },
+    { id: 3, name: "Carrera Exclusive Design Wheels", img: WheelsFilter_3 },
+    { id: 4, name: "Spyder Design wheels", img: WheelsFilter_4 },
+    { id: 5, name: "Turbo wheels", img: WheelsFilter_5 }
 ]
+import MaterialsFilter_1 from "/src/assets/images/MaterialsFilter_1.png";
+import MaterialsFilter_2 from "/src/assets/images/MaterialsFilter_2.png";
+import MaterialsFilter_3 from "/src/assets/images/MaterialsFilter_3.png";
+import MaterialsFilter_4 from "/src/assets/images/MaterialsFilter_4.png";
+import MaterialsFilter_5 from "/src/assets/images/MaterialsFilter_5.png";
 export const MaterialsFilter = [
-    { id: 1, name: "Mojave Beige two-tone leather", img: "../src/assets/images/MaterialsFilter_1.png" },
-    { id: 2, name: "Crayon two-tone leather", img: "../src/assets/images/MaterialsFilter_2.png" },
-    { id: 3, name: "Heritage Design Classic leather", img: "../src/assets/images/MaterialsFilter_3.png" },
-    { id: 4, name: "Bordeaux Red leather", img: "../src/assets/images/MaterialsFilter_4.png" },
-    { id: 5, name: "Truffle Brown Club", img: "../src/assets/images/MaterialsFilter_5.png" },
+    { id: 1, name: "Mojave Beige two-tone leather", img: MaterialsFilter_1 },
+    { id: 2, name: "Crayon two-tone leather", img: MaterialsFilter_2 },
+    { id: 3, name: "Heritage Design Classic leather", img: MaterialsFilter_3 },
+    { id: 4, name: "Bordeaux Red leather", img: MaterialsFilter_4 },
+    { id: 5, name: "Truffle Brown Club", img: MaterialsFilter_5 },
 ]
